c45b27f14386a444f71f6eb1247980fcaaab4653
ngonghia19092000 <73148099+ngonghia19092000@users.noreply.github.com>
2022-06-12 17:40:02 +0700

    setup basic

--- ATBM-Final/src/main/java/com/baokaka/controller/MainController.java
+++ ATBM-Final/src/main/java/com/baokaka/controller/MainController.java
@@ -1,5 +1,24 @@
 package com.baokaka.controller;
 
+import com.baokaka.model.Product;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
+import java.util.List;
+
+@Controller
 public class MainController {
+@RequestMapping({"danh-sach-san-pham","/productList"})
+public String showShoppingCart(Model model, Product product) {
+    return "productList";
+}
+    @RequestMapping({"","/home"})
+    public String showHome(Model model) {
+//        return "home";
+        return "productList";
+    }
 
 }
